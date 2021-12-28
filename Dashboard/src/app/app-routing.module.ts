292e36cbf5f3b0075677e8c7938e97c4cbe76b08
Nikita Basargin <nbasargin@web.de>
2021-12-28 13:29:49 +0100

    reformat routing

--- Dashboard/src/app/app-routing.module.ts
+++ Dashboard/src/app/app-routing.module.ts
@@ -1,28 +1,36 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { PlaygroundPageComponent } from './pages/playground-page/playground-page.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { StartComponent } from './pages/start/start.component';
-import { AuthGuardService } from './services/auth-guard.service';
-import { PrivateComponent } from './pages/private/private.component';
+import { RouterModule, Routes } from '@angular/router';
 import { MethodsComponent } from './pages/methods/methods.component';
+import { PlaygroundPageComponent } from './pages/playground-page/playground-page.component';
+import { StartComponent } from './pages/start/start.component';
 
 const routes: Routes = [
-  { path: '', component: PlaygroundPageComponent },
-  { path: 'zi-sim', component: StartComponent },
-  {
-    path: 'Methoden', component: MethodsComponent,
-  },
-  {
-    path: 'playground', component: PlaygroundPageComponent,
-  },
-  { path: '**', component: StartComponent }
+    {
+        path: '',
+        component: PlaygroundPageComponent
+    },
+    {
+        path: 'zi-sim',
+        component: StartComponent
+    },
+    {
+        path: 'Methoden',
+        component: MethodsComponent,
+    },
+    {
+        path: 'playground',
+        component: PlaygroundPageComponent,
+    },
+    {
+        path: '**',
+        component: StartComponent
+    }
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, {relativeLinkResolution: 'legacy'})],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
